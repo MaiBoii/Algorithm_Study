3714a8e763e7e489862ef89d2d3baa83b79ccef3
MaiBoii <20183010@gs.cwnu.ac.kr>
2023-03-27 00:34:33 +0900

    NM9

--- 백트래킹/N과M_9.py
+++ 백트래킹/N과M_9.py
@@ -1,16 +1,8 @@
 from itertools import permutations
 
 n,m = map(int, input().split())
-arr = sorted(list(map(int, input().split())))
-
-per = permutations(arr, m)
-tmp=[]
-for i in per:
-    if i not in tmp:
-        tmp.append(i)
-    continue
-
-for i in tmp:
-    for j in i:
-        print(j, end=' ')
-    print()
+arr = list(map(int, input().split()))
+arr = permutations(arr, m)
+arr = sorted(list(set(arr)))
+for i in arr:
+    print(*i)
